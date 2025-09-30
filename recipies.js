ae8b4499734a65405bab72b7a49f9c6464f516ea
aiden-original <mobereolanedia@gmail.com>
2025-09-30 14:35:52 +0100

    Add files via upload

---
+++ recipies.js
@@ -0,0 +1,71 @@
+
+recipies = [
+    {
+        "DishName":"pizza",
+        "Cuisine":"italy",
+        "Ingredience":{
+            "tomato":"2",
+            "apples":"4",
+            "flour":"100g"},
+        "Steps":["cut up all the veggies on a chopping board",
+            "boil some water and 200 degree and put the veggies in wait ten minutes",
+            "get 50g of flour and add butter etc..." ],
+        "Approved":"yes",
+        "Front_page":"https://img.freepik.com/premium-vector/salad-bowl-clipart-cartoon-style-vector-illustration_761413-15628.jpg",
+        "Background_colour":"grey",
+        "MaxPortion":"8"
+    },
+    {
+        DishName:"pizza",
+        Cuisine:"italy",
+        Ingredience:{
+            tomato:2,
+            apples:4,
+            flour:100+"g"},
+        Steps:["cut up all the veggies on a chopping board",
+            "boil some water and 200 degree and put the veggies in wait ten minutes",
+            "get 50g of flour and add butter etc..." ],
+        Approved:"yes",
+        Front_page:"",
+        Background_colour:"black",
+        MaxPortion:8
+        },
+    {
+        DishName:"pizza",
+        Cuisine:"italy",
+        Ingredience:{
+            tomato:2,
+            apples:4,
+            flour:100+"g"},
+        Steps:["cut up all the veggies on a chopping board",
+            "boil some water and 200 degree and put the veggies in wait ten minutes",
+            "get 50g of flour and add butter etc..." ],
+        Approved:"yes",
+        Front_page:"",
+        Background_colour:"red",
+        MaxPortion:8
+        },
+    {
+        DishName:"pizza",
+        Cuisine:"italy",
+        Ingredience:{
+            tomato:2,
+            apples:4,
+            flour:100+"g"},
+        Steps:["cut up all the veggies on a chopping board",
+            "boil some water and 200 degree and put the veggies in wait ten minutes",
+            "get 50g of flour and add butter etc..." ],
+        Approved:"yes",
+        Front_page:"",
+        Background_colour:"grey",
+        MaxPortion:8
+        },
+    
+    
+]
+
+
+urlsList = ["https://img.freepik.com/premium-vector/salad-bowl-clipart-cartoon-style-vector-illustration_761413-15628.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSA_f36nJiE8EgAN6t3EwMmwVl4Tp8oJTcPjQ&s",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwF5BpIjuqZ8V8c4D4WGFkHd11LVKC2ZZm3w&s",
+]
